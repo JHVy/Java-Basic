d74e7d658cd434a119c0310dc7e8d09420450794
JHVy <16521470@gm.uit.edu.vn>
2021-11-01 19:17:10 +0700

    update animal

--- Java-basic/src/Lesson_9_1/Animal.java
+++ Java-basic/src/Lesson_9_1/Animal.java
@@ -1,20 +1,12 @@
 package Lesson_9_1;
 
 public abstract class Animal {
-    protected int winCount = 0;
     iAnimal isRunning;
 
     protected abstract double velocity();
 
     protected abstract String name();
 
-    public int getWinCount() {
-        return winCount;
-    }
-
-    public void setWinCount(int winCount) {
-        this.winCount = winCount;
-    }
 
     protected boolean animalCanRun() {
         return isRunning.raceAble();
